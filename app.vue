7469f96938c97093e75e93dd4193c961ea05011a
gxmari007 <392041299@qq.com>
2022-05-08 22:28:51 +0800

    chore: 加入 UI 模块

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <n-layout>
+    <n-layout-header></n-layout-header>
+    <n-layout-content style="height: 220px;">
+      <n-layout-sider></n-layout-sider>
+    </n-layout-content>
+  </n-layout>
 </template>

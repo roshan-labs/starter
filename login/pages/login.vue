b74619579ab2f7da94cee79833a3c0bc05e716da
James Gao <392041299@qq.com>
2022-06-14 10:04:33 +0800

    chore: 更新依赖

--- login/pages/login.vue
+++ login/pages/login.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="w-380px p-sm">
-    <n-input placeholder="请输入" />
+    <n-space>
+      <n-input placeholder="请输入" />
+      <n-button>Hello {{ value }}</n-button>
+    </n-space>
   </div>
 </template>
+
+<script lang="ts" setup>
+const value = ref('hello123')
+</script>

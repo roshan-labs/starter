9cb1a0c9133a60780b540ddd9f1e6265d7159a7f
James Gao <392041299@qq.com>
2022-06-15 14:05:20 +0800

    chore: 修改路由

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,4 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
   modules: ['@roshan-labs/eslint-module', '@roshan-labs/ui-module'],
-  extends: ['./login'],
 })
--- pages/index.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    <div>Home page</div>
-    <nuxt-link to="/login">
-      <n-button type="primary">Login Page</n-button>
-    </nuxt-link>
-  </div>
-</template>
